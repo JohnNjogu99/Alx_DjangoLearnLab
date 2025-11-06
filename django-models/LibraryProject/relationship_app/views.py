42421880f049ce7a0f7452e7137173e04b175fe4
Johnnjogu <Johnnjogu99@gmail.comm>
2025-11-06 12:11:15 +0300

    Changed the view, urls and added library import

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Library
 from django.views.generic import DetailView
+from .models import Book, Library
 
 # Function-based View to list all books
 def list_books(request):
