42429421e0249d400b20905fccf0a88a0f44d543
Johnnjogu <Johnnjogu99@gmail.comm>
2025-11-12 16:16:52 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.utils import timezone
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
 
 # Step 3: Custom User Manager
 class CustomUserManager(BaseUserManager):
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+# bookshelf/views.py
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # logic to create a book
+    return render(request, 'create_book.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    # logic to edit book
+    return render(request, 'edit_book.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    # logic to delete book
+    return render(request, 'delete_book.html', {'book': book})
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'view_book.html', {'book': book})
+
